3c21418579b6b05b931a475274fb5a841f1e012b
brightestsirius <katherina.shypovskaya@gmail.com>
2023-12-05 17:06:13 +0200

    ts_intro.ts

--- ts_intro.ts
+++ ts_intro.ts
@@ -1,76 +1,84 @@
-let valueNumber:number = 10;
-let valueString:string = `str`;
-let valueBoolean:boolean = true;
-let valueUndefined:undefined = undefined;
-let valueNull:null = null;
-
-let arr_1: number[] = [10, 20, 30];
-let arr_2: (string | number)[] = [10, 'str'];
+let x:number = 10;
+let y: string = `hello`;
+let a: boolean = true;
+let u: undefined = undefined;
+let n: null = null;
 
 type StringOrNumber = string | number;
 
-let arr_3: StringOrNumber[] = [10, 'str'];
-let arr_4: Array<StringOrNumber> = [10, 'str'];
+let maybeString:StringOrNumber = 10;
+maybeString = `10`;
 
-interface IUser{
-    name: string;
-    age?: number;
-    animals: Array<string>
-}
+let arr_1: number[] = [10,20,30];
+let arr_2: (string | number)[] = [10,`20`];
+let arr_3: Array<StringOrNumber> = [10,`20`];
 
-let user:IUser = {
-    name: `User`,
-    animals: [`cat`, `dog`]
-}
+type RGBSchema = [number, number, number];
 
-type RGBcolor = [number, number, number];
+let color:RGBSchema = [255,255,255];
 
-let color: RGBcolor = [255, 255, 255];
-
-class Animal {
-    name: string = `default name`;
-
-    constructor(name: string = `default name`){
-        this.name = name;
-    }
-}
-
-class Tiger extends Animal{
-    type: string;
-
-    constructor(){
-        super();
-        this.type = `tiger`;
-    }
-}
-
-let lion = new Animal(`Simba`);
-let tiger = new Tiger();
-
-console.log(lion, tiger);
-
-enum RESPONSE_TYPES {
+enum REST {
     SUCCESS = `success`,
     ERROR = `error`
 }
 
 let response = {status: `success`};
 
-if(response.status === RESPONSE_TYPES.SUCCESS) console.log(RESPONSE_TYPES.SUCCESS)
-else console.log(RESPONSE_TYPES.ERROR);
+if(response.status === REST.ERROR) console.log(REST.ERROR)
+else console.log(REST.SUCCESS)
 
-const summ = (a:number, b?:number):number => {
+enum REST_METHODS {
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
+
+interface User{
+    name: string;
+    age?: number;
+    animals: Array<string>,
+    method: REST_METHODS[]
+}
+
+let user:User = {
+    name: `User`,
+    animals: [`cat`, `dog`],
+    method: [REST_METHODS.GET, REST_METHODS.DELETE]
+}
+
+class Animal{
+    public name: string;
+
+    constructor(name: string = `default name`){
+        this.name = name;
+    }
+}
+
+class Cat extends Animal{
+    getInfo(){
+        return `Hello, my name is ${this.name}`;
+    }
+}
+
+let Dog = new Animal(`Patron`);
+let Lion = new Animal();
+
+let Tom = new Cat();
+Tom.name = `Tom`;
+
+const sum = (a: number, b?: number): number => {
     if(b) return a+b;
     else return a;
 }
 
-summ(10, 20);
-summ(10);
+sum(10);
+sum(10, 20);
 
 const print = (value: string): void => console.log(value);
 
-const makeState = <T>(defaultState: T) => {
-    let state:T = defaultState;
+const makeState = <T>(defaultValue: T) => {
+    let state:T = defaultValue;
 
     const getState = () => state;
 
@@ -82,11 +90,37 @@ const makeState = <T>(defaultState: T) => {
 }
 
 let myState = makeState(1);
-
-myState.setState(1);
-console.log(myState.getState());
-
 myState.setState(2);
 console.log(myState.getState());
 
-export {}
+// Partial – some of options
+// Pick - only necessary options
+// Omit - without options
+
+interface Product{
+    id: number,
+    code: number,
+    name: string,
+    country: string
+}
+
+type PartialProduct = Partial<Product>
+type PickProduct = Pick<Product, "id" | "code">
+type OmitProduct = Omit<Product, "code" | "country">
+
+let product_1: PartialProduct = {
+    id: 1,
+    country: "Ukraine"
+}
+
+let product_2:PickProduct = {
+    id: 2,
+    code: 123
+}
+
+let product_3:OmitProduct = {
+    id: 2,
+    name: `phone`
+}
+
+export {};
